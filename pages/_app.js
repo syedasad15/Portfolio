a122be47c773cb92a8eb8264aa507c5f8ae3a3a4
syedasad15 <144409006+syedasad15@users.noreply.github.com>
2025-07-31 11:17:03 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { motion } from 'framer-motion';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
